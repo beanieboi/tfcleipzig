ae9d90c400205bdaf87f550e83b266c0a7eac786
ben <beanie@benle.de>
2019-03-10 20:52:51 +0100

    add deploy method

--- config.rb
+++ config.rb
@@ -5,37 +5,9 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
 
 activate :inline
 
@@ -45,4 +17,12 @@ configure :build do
   activate :asset_hash, ignore: %r{^assets/static/.*}
   # activate :gzip
   activate :minify_html
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method   = :ftp
+  deploy.host = 'tfc-leipzig.com'
+  deploy.path = '/web'
+  deploy.user = ENV.fetch('TFC_FTP_USER')
+  deploy.password = ENV.fetch('TFC_FTP_PASSWORD')
 end
